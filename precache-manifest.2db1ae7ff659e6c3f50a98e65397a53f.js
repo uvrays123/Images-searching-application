78b193ba0874bae9902bd878e6b5a1a5cb4eca19
uvrays123 <cserrao@umass.edu>
2020-09-02 17:35:02 +0530

    Updates

---
+++ precache-manifest.2db1ae7ff659e6c3f50a98e65397a53f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04a178eed9fa6d2e348d81a78fdcd852",
+    "url": "/unsplash-images-clone/index.html"
+  },
+  {
+    "revision": "1a47e04fd8f8c35b233c",
+    "url": "/unsplash-images-clone/static/css/main.7dd05744.chunk.css"
+  },
+  {
+    "revision": "d03f469416f10925cf0b",
+    "url": "/unsplash-images-clone/static/js/2.175df361.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/unsplash-images-clone/static/js/2.175df361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a47e04fd8f8c35b233c",
+    "url": "/unsplash-images-clone/static/js/main.26b009e5.chunk.js"
+  },
+  {
+    "revision": "5a5ef80760f609773018",
+    "url": "/unsplash-images-clone/static/js/runtime-main.84371783.js"
+  }
+]);
